73d5f124c2e7fdb9eac379c78f41eee3325f9409
JiaqiGao <jiaqigao12@gmail.com>
2018-01-09 19:03:24 -0600

    format

--- DerekEderstuff/d3.js
+++ DerekEderstuff/d3.js
@@ -1,87 +1,10 @@
 //import legend; //legend //'d3-svg-legend'
 
-  var year = null;
-  var width = 670,
-      height = 500;
 
-  var w = 300, h = 50;
-  var key = d3.select("#legend1")
-      .append("svg")
-      .attr("width", w)
-      .attr("height", h);
+///////////////////////////////////////////
 
-    var legend = key.append("defs")
-      .append("svg:linearGradient")
-      .attr("id", "gradient")
-      .attr("x1", "0%")
-      .attr("y1", "100%")
-      .attr("x2", "100%")
-      .attr("y2", "100%")
-      .attr("spreadMethod", "pad");
-
-    legend.append("stop")
-      .attr("offset", "0%")
-      .attr("stop-color", "#b2d4e6")//f7fcf0")
-      .attr("stop-opacity", 1);
-
-    legend.append("stop")
-      .attr("offset", "14%")
-      .attr("stop-color", "#328ebd")//bae4bc")
-      .attr("stop-opacity", 1);
-
-    legend.append("stop")
-      .attr("offset", "29%")
-      .attr("stop-color", "#0072ad")//7bccc4")
-      .attr("stop-opacity", 1);
-
-    legend.append("stop")
-      .attr("offset", "43%")
-      .attr("stop-color", "#005b8a")//084081")
-      .attr("stop-opacity", 1);
-
-
-    legend.append("stop")
-      .attr("offset", "58%")
-      .attr("stop-color", "#004467")//084081")
-      .attr("stop-opacity", 1);
-
-
-    legend.append("stop")
-      .attr("offset", "72%")
-      .attr("stop-color", "#0c3557")//084081")
-      .attr("stop-opacity", 1);
-
-    legend.append("stop")
-      .attr("offset", "86%")
-      .attr("stop-color", "#002d45")//084081")
-      .attr("stop-opacity", 1);
-
-    key.append("rect")
-      .attr("width", w)
-      .attr("height", h - 30)
-      .style("fill", "url(#gradient)")
-      .attr("transform", "translate(0,10)");
-
-    var y = d3.scaleLinear()
-      .range([300, 0])
-      .domain([700, 12]);
-
-    var yAxis = d3.axisBottom()
-      .scale(y)
-      .ticks(5);
-
-    key.append("g")
-      .attr("class", "y axis")
-      .attr("transform", "translate(0,30)")
-      .call(yAxis)
-      .append("text")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 0)
-      .attr("dy", ".71em")
-      .style("text-anchor", "end")
-    .text("axis title");
-
-
+var width = 670;
+var height = 500;
 
 var projection = d3.geoAlbers()
     .scale(70000)
@@ -92,7 +15,7 @@ var projection = d3.geoAlbers()
 var path = d3.geoPath()
     .projection(projection);
 
-var svg = d3.select("body").append("svg")
+var svg = d3.select(".last").append("svg")
     .attr("width", width)
     .attr("height", height);
 
@@ -103,7 +26,7 @@ var color = d3.scaleThreshold()
 
 
 
-var tooltip = d3.select("body")
+var tooltip = d3.select(".last")
     .append("div")
     .style("position", "absolute")
     .style("z-index", "10")
@@ -241,4 +164,4 @@ for (var i=0, len=clicks.length; i<len; i++) {
 */
 			//key.append("g").attr("class", "y axis").attr("transform", "translate(41,10)").call(yAxis).append("text").attr("transform", "rotate(-90)").attr("y", 30).attr("dy", ".71em").style("text-anchor", "end").text("axis title");
 
-//update();
+////////////////////////////////////////////////////
---
+++ DerekEderstuff/legend.js
@@ -0,0 +1,79 @@
+var year = null;
+  var width = 670,
+      height = 500;
+
+  var w = 300, h = 50;
+  var key = d3.select("#legend1")
+      .append("svg")
+      .attr("width", w)
+      .attr("height", h);
+
+    var legend = key.append("defs")
+      .append("svg:linearGradient")
+      .attr("id", "gradient")
+      .attr("x1", "0%")
+      .attr("y1", "100%")
+      .attr("x2", "100%")
+      .attr("y2", "100%")
+      .attr("spreadMethod", "pad");
+
+    legend.append("stop")
+      .attr("offset", "0%")
+      .attr("stop-color", "#b2d4e6")//f7fcf0")
+      .attr("stop-opacity", 1);
+
+    legend.append("stop")
+      .attr("offset", "14%")
+      .attr("stop-color", "#328ebd")//bae4bc")
+      .attr("stop-opacity", 1);
+
+    legend.append("stop")
+      .attr("offset", "29%")
+      .attr("stop-color", "#0072ad")//7bccc4")
+      .attr("stop-opacity", 1);
+
+    legend.append("stop")
+      .attr("offset", "43%")
+      .attr("stop-color", "#005b8a")//084081")
+      .attr("stop-opacity", 1);
+
+
+    legend.append("stop")
+      .attr("offset", "58%")
+      .attr("stop-color", "#004467")//084081")
+      .attr("stop-opacity", 1);
+
+legend.append("stop")
+    .attr("offset", "72%")
+    .attr("stop-color", "#0c3557")//084081")
+    .attr("stop-opacity", 1);
+
+legend.append("stop")
+    .attr("offset", "86%")
+    .attr("stop-color", "#002d45")//084081")
+    .attr("stop-opacity", 1);
+
+key.append("rect")
+    .attr("width", w)
+    .attr("height", h - 30)
+    .style("fill", "url(#gradient)")
+    .attr("transform", "translate(0,10)");
+
+var y = d3.scaleLinear()
+    .range([300, 0])
+    .domain([700, 12]);
+
+var yAxis = d3.axisBottom()
+    .scale(y)
+    .ticks(5);
+
+key.append("g")
+    .attr("class", "y axis")
+    .attr("transform", "translate(0,30)")
+    .call(yAxis)
+    .append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("y", 0)
+    .attr("dy", ".71em")
+    .style("text-anchor", "end")
+    .text("axis title");
